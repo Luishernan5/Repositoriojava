0f01e01f31e731c2f613767d3a4246d144e4b9be
Luis Hernan <ramirezluishernan5@gmail.com>
2025-02-12 23:41:58 -0600

    agregue el hola.java

---
+++ hola.java
@@ -0,0 +1,10 @@
+public class hola{
+    public static void main(String[] args){
+        System.out.println("HOLA MUNDO");
+        System.out.println("HOla Hernan");
+        
+        System.out.print("HOLA MUNDO");
+        System.out.println("");
+        System.out.println("Hola Hernan");
+    }
+}
