f6b1b4fa8a3ed6d0e06088451f89f4a81c90837d
Luis Hernan <ramirezluishernan5@gmail.com>
2025-02-26 23:20:46 -0600

    add

--- Proyectos_Java/ejercicio11_while.java
+++ Proyectos_Java/ejercicio11_while.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class ejercicio11_while{
     public static void main(String []args){
         try (/*
-                int i=1;
+                int i=1; 
                 while(i<10){
                     System.out.println("i="+i);
                     i+=1;
