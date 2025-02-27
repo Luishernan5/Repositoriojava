5c01c58d164f348a0ceeff00c410eb8b32ae3160
Luis Hernan <ramirezluishernan5@gmail.com>
2025-02-26 23:23:59 -0600

    add

---
+++ Proyectos_Java/ejercicio2_entradadedatosventanas.java
@@ -0,0 +1,27 @@
+
+import javax.swing.JOptionPane;
+
+public class ejercicio2_entradadedatosventanas{
+    public static void main(String []args){
+        //ingreso de una cadena
+        String cadena= JOptionPane.showInputDialog("Ingresa una cadena: ");
+        JOptionPane.showMessageDialog(null, "Esta es tu cadena: "+cadena);
+        
+        //ingreso de numero entero
+        int entero = Integer.parseInt(JOptionPane.showInputDialog("Dame un entero: "));
+        JOptionPane.showMessageDialog(null, "Este es tu numero: "+entero);
+        
+        //ingreso numero flotante
+        float flotante= Float.parseFloat(JOptionPane.showInputDialog("Dame un numero flotante: "));
+        JOptionPane.showMessageDialog(null, "Este es tu numero: "+flotante);
+        
+        //ingreso numero doble
+        double doble = Double.parseDouble(JOptionPane.showInputDialog("Dame un numero doble: "));
+        JOptionPane.showMessageDialog(null, "Este es tu numero; "+doble);
+        
+        //ingreso de caracteres
+        char caracter = JOptionPane.showInputDialog("Dame un caracter: ").charAt(2);
+        JOptionPane.showMessageDialog(null, "Tu caracter es: "+caracter);
+        
+    }
+}
---
+++ Proyectos_Java/ejercicio3_ventas.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class ejercicio3_ventas{
+    public static void main(String []args){
+        try (Scanner ventas = new Scanner(System.in)) {
+            System.out.println("Ingresa el valor del producto 1: ");
+            float prod1=ventas.nextFloat();
+            System.out.println("Ingresa el valor del producto 2: ");
+            float prod2=ventas.nextFloat();
+            System.out.println("Ingresa el valor del producto 3: ");
+            float prod3=ventas.nextFloat();
+            float suma = prod1+prod2+prod3;
+            System.out.println("El total de los productos es: "+suma);
+            double totalredondeado=Math.ceil(suma);
+            System.out.println("El total redondeado es: "+totalredondeado);
+            double redondeo=totalredondeado-suma;
+            System.out.println("Gracias por donar: "+redondeo);
+        }    
+    }
+}
