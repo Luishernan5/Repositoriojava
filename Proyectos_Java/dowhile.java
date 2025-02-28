ff24f7eb39d1c5eb69b282d1f0ba858969cf6350
Luis Hernan <ramirezluishernan5@gmail.com>
2025-02-27 22:25:03 -0600

    add

--- Proyectos_Java/dowhile.java
+++ Proyectos_Java/dowhile.java
@@ -1,9 +1,11 @@
-
+// importaciones:
 import java.util.Random;
 
 public class dowhile{
     public static void main(String []args){
+
         /*
+
         int i=1;
         do{
             System.out.println("i="+i);
